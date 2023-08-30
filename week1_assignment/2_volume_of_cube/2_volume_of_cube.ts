62d67027eefe2770724d62d28d9ca727278c67ea
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-30 09:26:53 +0400

    second assignment of first week

---
+++ week1_assignment/2_volume_of_cube/2_volume_of_cube.ts
@@ -0,0 +1,9 @@
+// Exercise 2: Write a program that takes input and calculates the volume of a cube.
+
+function volume_of_cube(a: number): void{
+    var volume = a*a*a
+    console.log("Volume of cube having side length",a, "Equal to" , volume);
+    
+}
+
+volume_of_cube(3)
