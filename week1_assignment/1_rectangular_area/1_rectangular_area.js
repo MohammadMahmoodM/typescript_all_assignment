3e22cb8897f254717e729dc9bab0d62c74ae89c2
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-30 09:08:49 +0400

    first assignment of first week

---
+++ week1_assignment/1_rectangular_area/1_rectangular_area.js
@@ -0,0 +1,6 @@
+// Exercise 1: Write a program that calculates the area of a rectangle.
+function rectangle_area(length, width) {
+    var rec_area = length * width;
+    console.log("a rectangular of length", length, "and width", width, "has an area =", rec_area);
+}
+rectangle_area(8, 4);
