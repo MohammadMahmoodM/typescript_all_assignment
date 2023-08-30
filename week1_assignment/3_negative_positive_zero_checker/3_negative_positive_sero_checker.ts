edf5e1b22419685b491e305b12dfe0d2eca703b6
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-08-30 09:31:43 +0400

    third assignment of first week

---
+++ week1_assignment/3_negative_positive_zero_checker/3_negative_positive_sero_checker.ts
@@ -0,0 +1,10 @@
+var given_number: number = 2
+
+if (given_number < 0){
+    console.log("Number is Negative");
+} else if (given_number == 0){
+    console.log("Number is zero");
+}else {
+    console.log("Number is Positive");
+    
+}
