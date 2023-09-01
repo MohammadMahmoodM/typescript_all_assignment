dde054159b907ae8998b3940d8a0aa00708875b9
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-09-01 12:18:52 +0400

    using map created new array multiple of 2

---
+++ week5_assignment/3_using_map_create_new_array_2_multiple/3_using_map_create_new_array_2_multiple.ts
@@ -0,0 +1,13 @@
+// filter reduce the length of array depending upon our requirements, but map make it stay same
+// map give array of same length but work accourding to our condition
+
+// Given an array of numbers [1, 2, 3, 4, 5], use the map method to create a new array
+// where each number is multiplied by 2
+
+var array_of_numbers : number[] = [1, 2, 3, 4, 5]
+console.log("Original Array", array_of_numbers);
+var empty_array: number[] = []
+array_of_numbers.map(element => {
+    empty_array.push(element * 2)
+})
+console.log("Multiply by 2 array",empty_array);
