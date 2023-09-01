70af6555fdd00eb81e9aecf5bb55ac8da738e4d4
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-09-01 10:16:11 +0400

    iteration using for loop

---
+++ week4_assignment/3_iteration_using_for_loop/iteration_usinf_for_loop_to_print_even.js
@@ -0,0 +1,7 @@
+//  Implement a program that uses a for loop to iterate through 
+// an array of numbers and print only the even numbers.
+var an_array_of_numbers = [1, 4, 7, 2, 6, 3, 0, 8, 10, 11, 14];
+for (var i = 0; i < an_array_of_numbers.length; i++) {
+    if (an_array_of_numbers[i] % 2 == 0)
+        console.log(an_array_of_numbers[i], "is even number");
+}
