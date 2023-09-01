d831b380550fa9b0a730232b8cb69f8c190bb83b
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-09-01 12:21:24 +0400

    array to get double of odd

---
+++ week5_assignment/6_array_to_get_double_of_odd/6_array_to_get_double_of_odd.ts
@@ -0,0 +1,14 @@
+// Given an array of numbers [3, 6, 9, 12, 15, 18], use the map and filter methods
+// together to create a new array containing the doubled values of odd numbers.
+
+var array_of_numbers : number[]= [3, 6, 9, 12, 15, 18]
+
+var new_array = array_of_numbers.filter(number => {
+    if(number%2 !=0)
+    return number
+}).map(num => {
+    num = num+num
+    return num
+})
+
+console.log("Array containing the doubled values of odd numbers",new_array);
