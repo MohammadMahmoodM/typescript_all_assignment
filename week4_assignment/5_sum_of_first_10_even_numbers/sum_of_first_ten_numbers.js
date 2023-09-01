ce338ad9bbaf401a675f806f1b0555b444c99764
MohammadMahmoodM <mohammadmahmood441@gmail.com>
2023-09-01 10:19:46 +0400

    sum of first 20 even numbers using loop

---
+++ week4_assignment/5_sum_of_first_10_even_numbers/sum_of_first_ten_numbers.js
@@ -0,0 +1,7 @@
+//Develop a program that calculates and prints the sum of the first n even numbers using 
+//a for loop.
+for (var i = 0; i < 1000000; i++) {
+    if (i % 2 == 0) {
+        console.log("".concat(i, " is even number"));
+    }
+}
---
+++ week4_assignment/5_sum_of_first_10_even_numbers/sum_of_first_ten_numbers.ts
@@ -0,0 +1,8 @@
+//Develop a program that calculates and prints the sum of the first n even numbers using 
+//a for loop.
+
+for (var i = 0; i<1000000 ; i++){
+    if(i%2 == 0){
+        console.log(`${i} is even number`);
+    }
+}
